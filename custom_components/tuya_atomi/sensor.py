f8e2ffd73c5290e02142eb76a10de79bf5dec504
o951753o <36664073+o951753o@users.noreply.github.com>
2023-01-07 07:53:31 -0800

    Update sensor.py

--- custom_components/tuya_atomi/sensor.py
+++ custom_components/tuya_atomi/sensor.py
@@ -510,7 +510,7 @@ SENSORS: dict[str, tuple[TuyaSensorEntityDescription, ...]] = {
             key=DPCode.TEMP_CURRENT,
             name="Temperature",
             icon="mdi:thermometer",
-            native_unit_of_measurement=UnitOfTemperature,
+            native_unit_of_measurement=FAHRENHEIT,
             device_class=SensorDeviceClass.TEMPERATURE,
             state_class=SensorStateClass.MEASUREMENT,
         ),
